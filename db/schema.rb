1daf1084df86660c35b92c46bc342a7eb42725c9
beth laing <laingbeth@gmail.com>
2015-04-07 16:36:33 -0400

    final fix to show

--- db/schema.rb
+++ db/schema.rb
@@ -11,41 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406151637) do
-
+ActiveRecord::Schema.define(version: 20_150_406_151_637) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "speakers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "headline"
-    t.string   "contact"
-    t.string   "topic"
-    t.text     "description"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+  create_table 'speakers', force: :cascade do |t|
+    t.string 'name'
+    t.string 'headline'
+    t.string 'contact'
+    t.string 'topic'
+    t.text 'description'
+    t.datetime 'created_at',          null: false
+    t.datetime 'updated_at',          null: false
+    t.string 'avatar_file_name'
+    t.string 'avatar_content_type'
+    t.integer 'avatar_file_size'
+    t.datetime 'avatar_updated_at'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                        null: false
-    t.string   "crypted_password",             null: false
-    t.string   "salt",                         null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email',                        null: false
+    t.string 'crypted_password',             null: false
+    t.string 'salt',                         null: false
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+    t.string 'remember_me_token'
+    t.datetime 'remember_me_token_expires_at'
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
+  add_index 'users', ['remember_me_token'], name: 'index_users_on_remember_me_token', using: :btree
 
-  create_table "welcomes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'welcomes', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
